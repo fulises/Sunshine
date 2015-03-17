36660aee48d4547e451ec51b7da116e5c8377fb7
Ulises E. Erickson <ulises.erickson@gmail.com>
2015-03-17 19:25:25 -0400

    3.4 ahora para el setting manageament wohiii, tal vez aprendo algo de embotellarmelo saludos

--- app/src/main/java/app/com/example/ulixes/sunshine/ForecastFragment.java
+++ app/src/main/java/app/com/example/ulixes/sunshine/ForecastFragment.java
@@ -15,6 +15,7 @@
  */
 package app.com.example.ulixes.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -29,7 +30,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -118,7 +118,11 @@ public class ForecastFragment extends Fragment {
                     @Override
              public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                  String forecast = forecastAdapter.getItem(position);
-                 Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(getActivity(),DetailActivity.class).putExtra(Intent.EXTRA_TEXT,forecast);
+                        startActivity(intent);
+
+
                  }
              });
 
@@ -279,7 +283,7 @@ public class ForecastFragment extends Fragment {
                 }
                 forecastJsonStr = buffer.toString();
 
-                
+
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
