c803f25f4b30025c2761a60e886fa149c4eabcc3
Ulises E. Erickson <ulises.erickson@gmail.com>
2015-03-18 18:52:25 -0400

    Manda el mensaje pero la cadena esta nulla deve ser ago que ver con el hecho que es una actividda y no un fragmento, revisaer y cambiar el codigo acorde

--- app/src/main/java/app/com/example/ulixes/sunshine/DetailActivity.java
+++ app/src/main/java/app/com/example/ulixes/sunshine/DetailActivity.java
@@ -3,7 +3,10 @@ package app.com.example.ulixes.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +16,11 @@ import android.widget.TextView;
 
 
 public class DetailActivity extends ActionBarActivity {
+
+    private static final String LOG_TAG = DetailActivity.class.getSimpleName();
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String mForecastStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +38,34 @@ public class DetailActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
+
+
+         // Retrieve the share menu item
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+    // Get the provider and hold onto it to set/change the share intent.
+    ShareActionProvider mShareActionProvider =
+                 (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+     // Attach an intent to this ShareActionProvider. You can update this at any time,
+       // like when the user selects a new piece of data they might like to share.
+   if (mShareActionProvider != null ) {
+    mShareActionProvider.setShareIntent(createShareForecastIntent());
+    } else {
+    Log.d(LOG_TAG, "Share Action Provider is null?");
+     }
+
         return true;
+
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 
     @Override
